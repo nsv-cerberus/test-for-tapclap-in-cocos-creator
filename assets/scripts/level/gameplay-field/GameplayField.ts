246909ca2556d7f513ddcbf9fefbc87164711c07
snesmian <snesmian@elince.md>
2026-06-29 06:04:01 +0300

    fix: resize GameplayField and Widgets

--- assets/scripts/level/gameplay-field/GameplayField.ts
+++ assets/scripts/level/gameplay-field/GameplayField.ts
@@ -3,20 +3,22 @@ const {ccclass} = cc._decorator;
 @ccclass
 export default class GameplayField extends cc.Component {
 
-    public resize(rows: number, cols: number, cellSize: cc.Size): void {
-        cc.log("Resize Gameplay Field: ", rows, cols, cellSize.width, cellSize.height);
+    public resize(rows: number, cols: number, cellSize: cc.Size): void {    
         this.resizeHeight(rows, cellSize);
         this.resizeWidth(cols, cellSize);
+
+        const widgets = this.node.getComponentsInChildren(cc.Widget);
+        for (const widget of widgets) {
+            widget.updateAlignment();
+        }
     }
     
     private resizeWidth(cols: number, cellSize: cc.Size): void {
         this.node.width = cols * cellSize.width + (cols - 1);
-        cc.log("GameplayField: resizeWidth: ", this.node.width);
     }
 
     private resizeHeight(rows: number, cellSize: cc.Size): void {
         this.node.height = rows * cellSize.height + (rows - 1);
-        cc.log("GameplayField: resizeHeight: ", this.node.height);
     }
     
 }
